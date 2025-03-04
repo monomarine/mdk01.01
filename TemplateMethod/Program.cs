200381e8015c3d18784bdac2b43e7b58ff1089ac
leray <lerayudanova05@gmail.com>
2025-03-04 13:05:48 +0500

    сделано задание

--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -5,18 +5,22 @@
         static void Main(string[] args)
         {
             Admin admin = new Admin();
-            Moderator moderator = new Moderator();  
+            Moderator moderator = new Moderator();
             Guest guest = new Guest();
+            VIPUser vip = new VIPUser();
 
             guest.Login = "testUser";
             moderator.Login = "Moderator121";
             admin.Login = "admin545";
+            vip.Login = "VIPUser222";
 
-            admin.Work("лого", moderator);
+            admin.Work("лого", moderator, vip);
             Console.WriteLine("-----------------------------------");
-            moderator.Work("flower" , guest);
+            moderator.Work("flower", guest, admin);
             Console.WriteLine("-----------------------------------");
-            guest.Work("car", admin);
+            guest.Work("car", admin, moderator);
+            Console.WriteLine("-----------------------------------");
+            vip.Work("ghghg", guest, admin);
         }
     }
 }
