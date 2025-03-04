4349b58a0ccb8d6a79465a982176b9623e8698ce
Саша Касаткин <virusgamers123231@gmail.com>
2025-03-04 13:29:34 +0500

    Касаткин

--- ChainOfResponsibility/Validators/EmailValidator.cs
+++ ChainOfResponsibility/Validators/EmailValidator.cs
@@ -9,6 +9,8 @@ namespace ChainOfResponsibility.Validators
     internal class EmailValidator : IValidator
     {
         private IValidator _nextValidator;
+        private readonly string[] _allowedDomains = { "mail.ru", "yandex.ru", "gmail.com" };
+
         public void SetNextValidator(IValidator validator)
         {
             _nextValidator = validator;
@@ -16,8 +18,33 @@ namespace ChainOfResponsibility.Validators
 
         public bool Validate(User user)
         {
-            if (String.IsNullOrEmpty(user.Email)) return false;
-            if(!user.Email.Contains("@")) return false;
+            if (String.IsNullOrEmpty(user.Email))
+            {
+                Console.WriteLine("Email не может быть пустым.");
+                return false;
+            }
+
+            if (!user.Email.Contains("@"))
+            {
+                Console.WriteLine("Email должен содержать символ '@'.");
+                return false;
+            }
+
+            var atIndex = user.Email.IndexOf('@');
+            var localPart = user.Email.Substring(0, atIndex);
+            var domain = user.Email.Substring(atIndex + 1);
+
+            if (localPart.Length < 8)
+            {
+                Console.WriteLine("Длина символов до '@' должна быть не менее 8.");
+                return false;
+            }
+
+            if (!_allowedDomains.Contains(domain))
+            {
+                Console.WriteLine("Доменное имя должно быть одним из: mail.ru, yandex.ru, gmail.com.");
+                return false;
+            }
 
             return _nextValidator?.Validate(user) ?? true;
         }
--- ChainOfResponsibility/Validators/PasswordValidator.cs
+++ ChainOfResponsibility/Validators/PasswordValidator.cs
@@ -10,6 +10,8 @@ namespace ChainOfResponsibility.Validators
     internal class PasswordValidator : IValidator
     {
         private IValidator _nextValidator;
+        private readonly char[] _specialChars = { '#', '*', '!', '@' };
+
         public void SetNextValidator(IValidator validator)
         {
             _nextValidator = validator;
@@ -17,12 +19,42 @@ namespace ChainOfResponsibility.Validators
 
         public bool Validate(User user)
         {
-            if (String.IsNullOrEmpty(user.Password) || user.Password.Length < 8)
+            if (String.IsNullOrEmpty(user.Password))
             {
-                Console.WriteLine("пароль не соответствует требуемой длине");
+                Console.WriteLine("Пароль не может быть пустым.");
                 return false;
             }
+
+            if (user.Password.Length < 8)
+            {
+                Console.WriteLine("Длина пароля должна быть не менее 8 символов.");
+                return false;
+            }
+
+            if (!user.Password.Any(c => _specialChars.Contains(c)))
+            {
+                Console.WriteLine("Пароль должен содержать хотя бы один из специальных символов: #, *, !, @.");
+                return false;
+            }
+
+            if (!user.Password.All(c => IsLatinLetter(c) || char.IsDigit(c) || _specialChars.Contains(c)))
+            {
+                Console.WriteLine("Пароль должен содержать только латинские символы, цифры и специальные символы.");
+                return false;
+            }
+
+            if (!user.Password.Any(char.IsDigit))
+            {
+                Console.WriteLine("Пароль должен содержать хотя бы одну цифру.");
+                return false;
+            }
+
             return _nextValidator?.Validate(user) ?? true;
+        }
+
+        private bool IsLatinLetter(char c)
+        {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
         }
     }
 }
